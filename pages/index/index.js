67d66cc69cd6c3e726d08a661f4f5f0d40f2bc25
pandoraemon <pz880304@163.com>
2018-01-20 16:04:54 +0800

    add sim.js package

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,16 +12,6 @@ Page({
     money: "1280",
   },
   //事件处理函数
-  toSetClock: function(){
-    wx.navigateTo({
-      url: '../setClock/setClock',
-    })
-  },
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
